4453e696caaccb77d69cc92cf9c9df0e25bb7f91
Evandro Marcel Lossavaro Martinelli <evandrom@MMB031776CPS.local>
2024-03-18 22:18:04 -0300

    Swagger Increment

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,14 +1,18 @@
-import { IsNumber, IsString, IsEmail, IsNotEmpty, IsDate, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsEmail, IsNotEmpty, IsDate, IsOptional } from 'class-validator';
 
 export class CreateUserDto {
+    @ApiProperty({ example: 'John Doe', description: 'The name of the User' })
     @IsString()
     @IsNotEmpty()
     name: string;
 
+    @ApiProperty({ example: 'email@email.com`', description: 'The email of the User' })
     @IsEmail()
     @IsNotEmpty()
     email: string;
 
+    @ApiProperty({ example: '12345678900', description: 'The cpf of the User' })
     @IsString()
     @IsNotEmpty()
     cpf: string;
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,16 +2,18 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
+  @ApiCreatedResponse({ type: CreateUserDto, description: 'User created' })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
-
+  @ApiOkResponse({ description: 'List of users'})
   @Get()
   findAll() {
     return this.userService.findAll();
