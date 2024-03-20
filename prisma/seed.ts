c44a729ab42975f605cc202155075505ba281644
Evandro Marcel Lossavaro Martinelli <evandrom@MMB031776CPS.local>
2024-03-20 00:28:00 -0300

    fix github actions

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-async function main() { }
+async function main() {
+  /* TODO document why this async function 'main' is empty */
+}
 main()
   .then(async () => {
     await prisma.$disconnect();
