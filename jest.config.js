c05c4b69c5599535c5317d3da462125033c6d15d
Evandro Martinelli <evandro@evandro>
2024-03-22 17:56:44 -0300

    quality

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,40 @@
 module.exports = {
-    moduleFileExtensions: [
-        'js',
-        'json',
-        'ts'
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: [
+    '.*entity\\.ts$',
+    '.*module\\.ts$',
+    'src/main.ts',
+  ],
+  coverageReporters: [
+    'html',
+    'text',
+    'text-summary',
+    'cobertura',
+    'clover',
+    'json',
+    'lcov',
+  ],
+  collectCoverage: true,
+  eporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        classNameTemplate: `{classname}`,
+        titleTemplate: '{title}',
+        ancestorSeparator: ' › ',
+        suiteNameTemplate: `${process.env.npm_package_name}`,
+        includeConsoleOutput: true,
+        addFileAttribute: 'true',
+      },
     ],
-    rootDir: 'src',
-    testRegex: '.*\\.spec\\.ts$',
-    transform: {
-        '^.+\\.(t|j)s$': 'ts-jest'
-    },
-    collectCoverageFrom: [
-        '**/*.(t|j)s'
-    ],
-    coverageDirectory: '../coverage',
-    testEnvironment: 'node',
-    coveragePathIgnorePatterns: [
-        '.*entity\\.ts$',
-        '.*module\\.ts$',
-        'src/main.ts'
-    ]
+  ],
 };
