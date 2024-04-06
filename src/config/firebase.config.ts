9a6aa9a617524c1b287a3dd34b07c5df43bd60d7
Evandro Martinelli <evandro@evandro>
2024-04-06 19:20:47 -0300

    firebase env config

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,5 +1,14 @@
 import * as admin from 'firebase-admin';
-import serviceAccount from './profitei-firebase-adminsdk-ikxkn-5378e2c303.json';
+
+const serviceAccount = process.env.FIREBASE_ADMIN_SDK
+  ? JSON.parse(process.env.FIREBASE_ADMIN_SDK)
+  : undefined;
+
+if (!serviceAccount) {
+  throw new Error(
+    'A variável de ambiente FIREBASE_ADMIN_SDK não está definida.',
+  );
+}
 
 const firebase = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
