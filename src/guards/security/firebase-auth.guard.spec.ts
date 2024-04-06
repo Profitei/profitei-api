80133c9ec234eeedfddeeaf4d39466865dc14bd3
Evandro Martinelli <evandro@evandro>
2024-04-06 20:10:26 -0300

    fix tests

--- src/guards/security/firebase-auth.guard.spec.ts
+++ src/guards/security/firebase-auth.guard.spec.ts
@@ -1,7 +1,16 @@
 import { FirebaseAuthGuard } from './firebase-auth.guard';
 
-describe('SecurityGuard', () => {
+// Mock do Firebase
+jest.mock('../../config/firebase.config', () => ({
+  auth: () => ({
+    verifyIdToken: jest.fn(() => Promise.resolve(true)), // Assume um token válido por padrão
+  }),
+}));
+
+describe('FirebaseAuthGuard', () => {
   it('should be defined', () => {
     expect(new FirebaseAuthGuard()).toBeDefined();
   });
+
+  // Adicione aqui mais testes conforme necessário
 });
