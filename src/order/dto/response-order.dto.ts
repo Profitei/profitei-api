b3999ebc3e93b1e1856412d067825054c42a0886
Evandro Martinelli <evandro@evandro>
2024-04-09 00:17:58 -0300

    checkout api

--- src/order/dto/response-order.dto.ts
+++ src/order/dto/response-order.dto.ts
@@ -3,13 +3,15 @@ export class ResponseOrderDto {
   orderPrice: number;
   pixData: PixData;
   constructor(data: any) {
-    this.tickets = data.tickets.map((ticket) => ({
-      id: ticket.id,
-      name: ticket.Raffle.name,
-      Raffle: {
-        price: ticket.Raffle.price,
-      },
-    }));
+    this.tickets = data.tickets.map(
+      (ticket: { id: any; Raffle: { name: any; price: any } }) => ({
+        id: ticket.id,
+        name: ticket.Raffle.name,
+        Raffle: {
+          price: ticket.Raffle.price,
+        },
+      }),
+    );
     this.orderPrice = this.calculateOrderPrice(data.tickets);
     this.pixData = {
       qrCode: data.paymentData.point_of_interaction.transaction_data.qr_code,
@@ -18,10 +20,7 @@ export class ResponseOrderDto {
     };
   }
   private calculateOrderPrice(tickets: Tickets[]): number {
-    // Implementação da lógica de cálculo do preço total dos tickets
     return tickets.reduce((total, ticket) => {
-      // Supondo que cada ticket tenha um preço que você deseja somar
-      // Aqui você precisará ajustar de acordo com a estrutura real do seu objeto Ticket
       return total + (ticket.Raffle.price ?? 0);
     }, 0);
   }
