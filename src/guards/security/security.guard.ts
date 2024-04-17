3d8e6c39c09b9d1d7ba9fd51b43a1d15541df25d
Evandro Martinelli <evandro@evandro>
2024-04-17 14:58:02 -0300

    log guard

--- src/guards/security/security.guard.ts
+++ src/guards/security/security.guard.ts
@@ -2,24 +2,30 @@ import {
   CanActivate,
   ExecutionContext,
   Injectable,
+  Logger,
   UnauthorizedException,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class SecurityGuard implements CanActivate {
+  private readonly logger = new Logger(SecurityGuard.name);
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
 
-    const apiKey = request.headers['api-key']; // give the name you want
+    const apiKey = request.headers['api-key'];
+
+    this.logger.log('Validating API key');
 
     if (!apiKey) {
       throw new UnauthorizedException('API key is missing.');
     }
 
     if (apiKey !== process.env.API_KEY) {
+      this.logger.log(apiKey);
+      this.logger.log(process.env.API_KEY);
       throw new UnauthorizedException('Invalid API key.');
     }
 
