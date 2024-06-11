cfa3491e248e8a1c49b7e6278c83d71f0d25a393
Evandro Martinelli <evandro@evandro>
2024-06-11 11:15:04 -0300

    fix: allow noauth webhook

--- src/webhook/webhook.controller.ts
+++ src/webhook/webhook.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { PaymentService } from '../payment/payment.service';
+import { NoAuth, Public } from 'src/decorators/public.decorator';
 
 @Controller('webhook')
+@Public()
+@NoAuth()
 export class WebhookController {
   constructor(private readonly paymentService: PaymentService) {}
 
