afd629f5615b68a929948b03f0622329dc90844f
Evandro Martinelli <evandro@evandro>
2024-06-11 17:11:24 -0300

    feat: mercado pago guard

--- src/guards/security/mercado-pago.guard.ts
+++ src/guards/security/mercado-pago.guard.ts
@@ -15,50 +15,51 @@ export class MercadoPagoGuard implements CanActivate {
 
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
-    return validateRequest(request);
+    return this.validateRequest(request);
   }
-}
-function validateRequest(request: any): boolean {
-  this.logger.log('Validating Mercado Pago Webhook request');
 
-  // Assuming headers is an object containing request headers
-  const { 'x-signature': xSignature, 'x-request-id': xRequestId } =
-    request.headers;
+  private validateRequest(request: any): boolean {
+    this.logger.log('Validating Mercado Pago Webhook request');
 
-  // Obtain Query params related to the request URL
-  const urlParams = new URLSearchParams(window.location.search);
-  const dataID = urlParams.get('data.id');
+    // Assuming headers is an object containing request headers
+    const { 'x-signature': xSignature, 'x-request-id': xRequestId } =
+      request.headers;
 
-  // Separating the x-signature into parts
-  const parts = xSignature.split(',');
+    // Obtain Query params related to the request URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const dataID = urlParams.get('data.id');
 
-  // Initializing variables to store ts and hash
-  let ts;
-  let hash;
+    // Separating the x-signature into parts
+    const parts = xSignature.split(',');
 
-  // Iterate over the values to obtain ts and v1
-  parts.forEach((part) => {
-    const [key, value] = part.split('=').map((str) => str.trim());
-    if (key === 'ts') {
-      ts = value;
-    } else if (key === 'v1') {
-      hash = value;
-    }
-  });
+    // Initializing variables to store ts and hash
+    let ts;
+    let hash;
 
-  // Obtain the secret key for the user/application from Mercadopago developers site
-  const secret = process.env.MP_SECRET_SIGN;
+    // Iterate over the values to obtain ts and v1
+    parts.forEach((part) => {
+      const [key, value] = part.split('=').map((str) => str.trim());
+      if (key === 'ts') {
+        ts = value;
+      } else if (key === 'v1') {
+        hash = value;
+      }
+    });
 
-  // Generate the manifest string
-  const manifest = `id:${dataID};request-id:${xRequestId};ts:${ts};`;
+    // Obtain the secret key for the user/application from Mercadopago developers site
+    const secret = process.env.MP_SECRET_SIGN;
 
-  // Create an HMAC signature
-  const hmac = createHmac('sha256', secret);
-  hmac.update(manifest);
+    // Generate the manifest string
+    const manifest = `id:${dataID};request-id:${xRequestId};ts:${ts};`;
 
-  // Obtain the hash result as a hexadecimal string
-  const sha = hmac.digest('hex');
+    // Create an HMAC signature
+    const hmac = createHmac('sha256', secret);
+    hmac.update(manifest);
 
-  // Check if the generated HMAC matches the provided hash
-  return sha === hash;
+    // Obtain the hash result as a hexadecimal string
+    const sha = hmac.digest('hex');
+
+    // Check if the generated HMAC matches the provided hash
+    return sha === hash;
+  }
 }
