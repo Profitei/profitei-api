5a7c3886bf31dd727e2eb55c38df405f92232d15
Evandro Martinelli <evandro@evandro>
2024-08-20 16:21:10 -0300

    fix orderdto param

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,6 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { ArrayNotEmpty, IsArray, IsNumber } from 'class-validator';
 
 export class CreateOrderDto {
   @ApiProperty({ example: [1, 2, 3] })
+  @ApiProperty({ example: [1, 2, 3] })
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsNumber({}, { each: true })
   ticketsId: number[];
 }
